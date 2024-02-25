af9a2bdce728ea2a08b13d1ca460bcaea3455676
Marc Hines <marchines99@gmail.com>
2024-02-24 16:09:06 -0800

    Relocate Flasher source files

---
+++ TurnOut/Flasher.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include "Flasher.h"
+
+Flasher::Flasher(int pin, long on, long off)
+  {
+	ledPin = pin;
+	pinMode(ledPin, OUTPUT);     
+	  
+	OnTime = on;
+	OffTime = off;
+	
+	ledState = LOW; 
+	previousMillis = 0;
+  }
+
+  void Flasher::Update()
+  {
+    // check to see if it's time to change the state of the LED
+    unsigned long currentMillis = millis();
+     
+    if((ledState == HIGH) && (currentMillis - previousMillis >= OnTime))
+    {
+    	ledState = LOW;  // Turn it off
+      previousMillis = currentMillis;  // Remember the time
+      digitalWrite(ledPin, ledState);  // Update the actual LED
+    }
+    else if ((ledState == LOW) && (currentMillis - previousMillis >= OffTime))
+    {
+      ledState = HIGH;  // turn it on
+      previousMillis = currentMillis;   // Remember the time
+      digitalWrite(ledPin, ledState);	  // Update the actual LED
+    }
+  }
